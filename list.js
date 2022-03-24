a5e3185688726c49cf51758e430dedc3b7dd5e00
Yanius <zhzh-1987@mail.ru>
2022-03-24 23:48:14 +0700

    list.commit1

--- list.js
+++ list.js
@@ -1,7 +1,7 @@
-let data = [['Adam', 'doctor', 32], ['Maria', 'teacher', 27], ['Leon', 'manager', 23], ['Mattew', 'builder', 35]];
+let data = ['Adam', 'doctor', 32, 'Maria', 'teacher', 27, 'Leon', 'manager', 23, 'Mattew', 'builder', 35];
 //Должна выводить в консоль только числа из массива
 let numbers = [];
 data.forEach(item => {
   if(typeof(item) === 'number') numbers.push(item);
-  console.log(numbers);
   });
+  console.log(numbers);
