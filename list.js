dc4ddd72564a55c98acea762c363b2ca6d453537
Yanius <zhzh-1987@mail.ru>
2022-03-24 23:43:02 +0700

    git

--- list.js
+++ list.js
@@ -1,11 +1,7 @@
 let data = [['Adam', 'doctor', 32], ['Maria', 'teacher', 27], ['Leon', 'manager', 23], ['Mattew', 'builder', 35]];
 //Должна выводить в консоль только числа из массива
-function isNumber(value) {
-  for(let i = 0; i < 4; i ++){
-    for(let n = 0; n < 3; n ++){
-      if(typeof (value[i][n]) === 'number') console.log(value[i][n]);
-      else continue;
-    }
-  }
-}
-isNumber(data);
+let numbers = [];
+data.forEach(item => {
+  if(typeof(item) === 'number') numbers.push(item);
+  console.log(numbers);
+  });
