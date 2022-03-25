88a5913263dc201b1861782d6b688364947844ee
Yanius <zhzh-1987@mail.ru>
2022-03-25 21:03:18 +0700

    list.trueCommit

--- list.js
+++ list.js
@@ -1,7 +1,17 @@
 let data = [['Adam', 'doctor', 32], ['Maria', 'teacher', 27], ['Leon', 'manager', 23], ['Mattew', 'builder', 35]];
 //Должна выводить в консоль только числа из массива
+
 let numbers = [];
-data.forEach(item => {
-  if(typeof(item) === 'number') numbers.push(item);
-  });
-  console.log(numbers);
+//новый массив для возрастов
+
+function pushElem(value) {
+  typeof(value) === 'number' && numbers.push(value);
+}//функция которая проверяет, является ли элемент числом и пушит его в новый массив, если да
+
+function findArray(value) {
+  Array.isArray(value) && value.forEach(pushElem);
+}//проверяет,является ли элемент массивом и вызывает коллбэк, если да
+
+data.forEach(findArray);
+//метод для двухмерного массива
+console.log(numbers);
