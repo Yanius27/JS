ea56e75d69d69c11acafc13549d4b1b25e05c4fc
Yanius <zhzh-1987@mail.ru>
2022-03-27 09:15:58 +0700

    commit1

---
+++ orders.js
@@ -0,0 +1,36 @@
+const orders = [ 
+{
+  id: 1,
+  isActiv: true,
+  child: [ 
+  {
+    title: 'test',
+    isActiv: true,
+    child: [ 
+    {
+      title: 'test',
+      isActiv: false,
+      child: []
+    } 
+    ]
+  }
+  ]
+},
+{
+  id: 2
+  isActiv: false,
+  child: [
+  {
+    title: 'test2',
+    isActiv: false,
+    child: [
+    {
+      title: 'test',
+      isActiv: true,
+      child: []    
+    }    
+    ]
+  }      
+  ]  
+}
+]
