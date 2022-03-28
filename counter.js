364396a7656f6995b8a33d13299a8f7115e1fe83
Yanius <zhzh-1987@mail.ru>
2022-03-29 00:19:20 +0700

    commit3

---
+++ counter.js
@@ -0,0 +1,19 @@
+/*function counter(number) {
+    if(number <= 5) {
+        console.log(number);
+        const newNumber = number + 1;
+        counter(newNumber);
+    } else {
+        console.log('Its all');
+    } ;
+}
+counter(1);*/
+
+let applicants = [['Adam Parker', 3], ['Ned Kelly', 4], ['Saorsia Spinning', 5], ['Charlie Jhones', 5], ['Samantha M'. 'Strong', 4], ['Simon Drake', 3], ['Mindie Takker', 3], ['Yan Shein', 4], ['Trevor Lloyd', 5], ['Lora Shablinski', 4]];
+
+let students = [];
+
+
+
+
+
