e24212a0622c221ee491f4cf9ce6c0bdbb0a15aa
Yanius <zhzh-1987@mail.ru>
2022-03-29 16:35:32 +0700

    1commit

---
+++ cb.js
@@ -0,0 +1,27 @@
+const applications = [
+  {id: 1, isActiv: true},
+  {id: 2, isActiv: false},
+  {id: 3, isActiv: true},
+  {id: 4, isActiv: true},
+  {id: 5, isActiv: false},
+  {id: 6, isActiv: false},
+  {id: 7, isActiv: true},
+  {id: 8, isActiv: false},
+  {id: 9, isActiv: true},
+  {id: 10, isActiv: false},
+];
+
+let app = [];
+let unApp = [];
+function isApprovedOrNot(elem) {
+  elem.isActiv === true ? app.push(elem) : unApp.push(elem);
+}
+
+function mainFunc(arr, cb, elem) {
+  Array.isArray(elem) && elem.forEach((elem) => isApprovedOrNot(elem)); 
+}
+
+mainFunc(applications, isApprovedOrNot);
+
+console.log(app);
+console.log(unApp);
