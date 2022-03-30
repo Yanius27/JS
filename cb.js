532b5994d82b3b54433d64068a62524d76ed2491
Yanius <zhzh-1987@mail.ru>
2022-03-30 14:03:44 +0700

    lastCommit

--- cb.js
+++ cb.js
@@ -9,21 +9,22 @@ const applications = [
   {id: 8, isActiv: false},
   {id: 9, isActiv: true},
   {id: 10, isActiv: false},
-];
+]; //Массив объектов с заявками
 
-let app = [];
-let unApp = [];
-function isApproved(elem) {app.push(elem)};
+let app = []; //Новый массив для активных заявок
+let unApp = []; //Новый массив для неактивных заявок
 
-function orNotApproved(elem) {unApp.push(elem)};
+function isApproved(elem) {app.push(elem)}; //1-ая функция коллбэк
+
+function orNotApproved(elem) {unApp.push(elem)}; //2-ая функция коллбэк
 
 function mainFunc(arr, cb1, cb2) {
-  Array.isArray(arr) && arr.forEach(function status(arr) {
-    arr.isActiv === true ? cb1 : cb2;
+  Array.isArray(arr) && arr.forEach(function status(elem) {
+    elem.isActiv === true ? cb1(elem) : cb2(elem);
   }); 
-}
+} //Основная функция с проверками, методом массива и вызовом коллбэков
 
-mainFunc(applications, isApproved, orNotApproved);
+mainFunc(applications, isApproved, orNotApproved); //Вызов основной функции
 
 console.log(app);
 console.log(unApp);
