e3100fd98bfa4c1cd16d7306895621582da666c9
Yanius <zhzh-1987@mail.ru>
2022-04-01 00:10:38 +0700

    trueCommit

--- entrants.js
+++ entrants.js
@@ -2,14 +2,14 @@ let entrants = [['Adam Parker', 3], ['Ned Kelly', 4], ['Saorsia Spinning', 5], [
 
 let students =[];
 
-function congrats (elem) {
-typeof elem === 'number' && elem >  3 && students.push(elem[0]);
+function insideList(elem) {
+  elem.length === 2 && typeof elem[0] === 'string' && elem[1] > 3 && students.push(elem[0]);
 } //проверяет, является ли элемент числом и если он больше 3, добавляет строку из данного массива в новый массив
 
-function isListRec (elem) {
-if(Array.isArray(elem)) ? entrants.forEach(isListRec) : congrats;
+function listRec(elem) {
+  Array.isArray(elem) && elem.forEach(insideList);
 } //проверяет, является ли элемент массивом и запускает метод
 
-isListRec(entrants);
+listRec(entrants);
 
 console.log(students);
