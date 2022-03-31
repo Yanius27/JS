bf1d99cb24e2ae19514cb61077dcd3b3cef80d78
Yanius <zhzh-1987@mail.ru>
2022-04-01 00:20:29 +0700

    trueCommit

--- list.js
+++ list.js
@@ -4,14 +4,14 @@ let data = [['Adam', 'doctor', 32], ['Maria', 'teacher', 27], ['Leon', 'manager'
 let numbers = [];
 //новый массив для возрастов
 
-function pushElem(value) {
-  typeof(value) === 'number' && numbers.push(value);
+function insideArray(value) {
+  Array.isArray(value) && numbers.push(value[2]);
 }//функция которая проверяет, является ли элемент числом и пушит его в новый массив, если да
 
 function findArray(value) {
-  Array.isArray(value) && value.forEach(pushElem);
+  Array.isArray(value) && value.forEach(insideArray);
 }//проверяет,является ли элемент массивом и вызывает коллбэк, если да
 
-data.forEach(findArray);
-//метод для двухмерного массива
+findArray(data);
+//Первая функция
 console.log(numbers);
