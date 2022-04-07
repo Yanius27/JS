3b6aabb707e8358d18f4b0e5c30bf07bb3d21d87
Yanius <zhzh-1987@mail.ru>
2022-04-08 02:26:35 +0700

    1commit

---
+++ cbV2.js
@@ -0,0 +1,24 @@
+const applications = [
+    {id: 1, isActiv: true},
+    {id: 2, isActiv: false},
+    {id: 3, isActiv: true},
+    {id: 4, isActiv: true},
+    {id: 5, isActiv: false},
+    {id: 6, isActiv: false},
+    {id: 7, isActiv: true},
+    {id: 8, isActiv: false},
+    {id: 9, isActiv: true},
+    {id: 10, isActiv: false},
+  ]; //Массив объектов с заявками
+
+/*const resetStatus = applications.map(elem => {
+  elem.isActiv = 'false';
+  return elem;
+});*/
+
+const onlyActiv = applications.filter(elem => {
+  return elem.isActiv == 'true';
+});
+
+//console.log(resetStatus);
+console.log(onlyActiv);
