8d5cfeed3d054384d513b7ade2d5664d03e9ae45
Yanius <zhzh-1987@mail.ru>
2022-04-10 01:09:10 +0700

    trueCommit

--- cbV2.js
+++ cbV2.js
@@ -11,14 +11,20 @@ const applications = [
     {id: 10, isActiv: false},
   ]; //Массив объектов с заявками
 
-/*const resetStatus = applications.map(elem => {
-  elem.isActiv = 'false';
-  return elem;
-});*/
+const resetStatus = applications.map(elem => ({
+  ...elem,
+  isActiv: false,
+  date: new Date(),
+})); //С помощью метода .map и spread-оператора создаём новый массив со
+//сброшенными статусами и новым свойством Date
 
-const onlyActiv = applications.filter(elem => {
-  return elem.isActiv == 'true';
-});
+const onlyActiv = applications.filter(elem => elem.isActiv).map(elem => ({
+  ...elem,
+  date: new Date(),
+})); //С помощью метода .filter отбираем объекты со свойством isActiv: true,
+//с помощью метода .map и spread-оператора создаём новый массив с отфильтрованными
+//объектами и добавляем новое свойство Date
 
-//console.log(resetStatus);
+console.log(applications);
+console.log(resetStatus);
 console.log(onlyActiv);
