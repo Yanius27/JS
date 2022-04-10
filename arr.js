e1aab591264be1d229789de3f653027c5b1b0cdc
Yanius <zhzh-1987@mail.ru>
2022-04-10 20:10:37 +0700

    1commit

---
+++ arr.js
@@ -0,0 +1,21 @@
+const arr = [[{id: 1.1, isActiv: true,}, {id: 1.2, isActiv: true,}], [{id: 2.1, isActiv: false,}], [{id: 3.1, isActiv: true,}, {id: 3.2, isActiv: false}], [{id: 4.1, isActiv: true}]];
+
+let garbage = [];
+let approved =[];
+
+function search(elem) {
+  Array.isArray(elem) ? elem.forEach(search) : objFunc(elem);
+}
+
+function objFunc(elem) {
+  elem.isActiv === true ? (elem.status = 'approved') && (elem.date = new Date()) && (approved.push(elem)) : garbage.push(elem);
+}
+
+search(arr);
+
+console.log('                                 ');
+console.log('=========approved array=========');
+console.log(approved);
+console.log('                                 ');
+console.log('==========garbage array==========');
+console.log(garbage);
